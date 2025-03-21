10cbb11a5a3bf4a6c3f12b0504f3d479b3261b2d
Santawat Santiteerakul <santawat.s@hiveground.com>
2025-03-21 21:02:21 +0700

    Add vim-tmux-navigator

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -1,4 +1,3 @@
-print 'from keymap'
 return {
   vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Open Explorer' }),
   vim.keymap.set('n', '<leader>tn', vim.cmd.tabnew, { desc = '[T]ab [N]ew' }),
